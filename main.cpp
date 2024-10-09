32f8f0120988dacd302166b15e994698e8b84e49
anzipex <anzipex@gmail.com>
2024-10-09 18:43:17 +0300

    default window position

--- main.cpp
+++ main.cpp
@@ -112,7 +112,6 @@ void Display(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_MULTISAMPLE);
     glutInitWindowSize(WinWidth, WinHeight);
-    glutInitWindowPosition(320, 180);
     glutCreateWindow("Object Control");
     glutDisplayFunc(DisplayFunc);
     glutIdleFunc(IdleFunc);
